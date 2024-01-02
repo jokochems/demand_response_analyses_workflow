bd70ff6786dc6801d8b015c510362a78fb6d620b
Johannes Kochems <johannes.kochems@dlr.de>
2024-01-02 17:18:16 +0100

    Include variable shifting costs in evaluation

--- dispatch_inspection.py
+++ dispatch_inspection.py
@@ -1,6 +1,7 @@
 import math
 from typing import Dict
 
+import numpy as np
 import pandas as pd
 
 from dr_analyses.time import cut_leap_days
@@ -64,6 +65,36 @@ def set_time_index(combined: pd.DataFrame):
     combined.set_index("new_index", inplace=True)
 
 
+def retreive_variable_shifting_costs(
+    config_dispatch: Dict,
+    cluster: str,
+    scenario: str,
+    combined_results: pd.DataFrame,
+) -> pd.DataFrame:
+    """Retreive the variable shifting costs for given cluster and scenario"""
+    variable_costs = pd.read_csv(
+        f"{config_dispatch['input_folder']}"
+        f"{config_dispatch['data_sub_folder']}/"
+        f"{cluster}/{scenario}/{cluster}_variable_costs.csv",
+        sep=";",
+        index_col=0,
+        header=None,
+    )
+    variable_costs.index = pd.to_datetime(
+        variable_costs.index.str.replace("_", " ")
+    )
+    combined_results["VariableShiftingCosts"] = variable_costs[1]
+    years = math.floor(combined_results.shape[0] / 8760)
+    combined_results.loc[f"{2020 + years}-01-01 00:00:00"] = np.nan
+    combined_results.at[
+        f"{2020 + years}-01-01 00:00:00", "VariableShiftingCosts"
+    ] = variable_costs.loc[f"{2020 + years}-01-01 00:00:00"]
+    combined_results["VariableShiftingCosts"] = combined_results[
+        "VariableShiftingCosts"
+    ].interpolate()
+    return combined_results[:-1]
+
+
 if __name__ == "__main__":
     args = add_args()
     config_file = load_yaml_file(args.file)
@@ -71,6 +102,13 @@ if __name__ == "__main__":
 
     for cluster, tariffs in config_dispatch["cases"].items():
         for tariff in tariffs:
-            combined_results = read_and_combine_results(
+            combined_results_raw = read_and_combine_results(
                 config_dispatch, cluster, tariff
             )
+            combined_results = retreive_variable_shifting_costs(
+                config_dispatch,
+                cluster,
+                tariff["scenario"],
+                combined_results_raw,
+            )
+            print("stop")
